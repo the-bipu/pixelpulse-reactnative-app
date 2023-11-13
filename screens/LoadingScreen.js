5ce05f118a81bd7fc22bbd15e967c83b0fd5b4a7
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-14 03:25:05 +0530

    Update LoadingScreen.js

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, ImageBackground, Image } from 'react-native'
+import { View, Text, SafeAreaView, ImageBackground, Image, TouchableOpacity } from 'react-native'
 import React, { useEffect, useLayoutEffect } from 'react'
 import * as Animatable from 'react-native-animatable'
 // import * as Progress from 'react-native-progress'
@@ -13,6 +13,10 @@ const LoadingScreen = () => {
           headerShown: false,
         });
     }, []);
+
+    const handlePress = () => {
+      navigation.navigate('HomeScreen');
+    };
       
     // useEffect(() => {
     //     setTimeout(() => {
@@ -23,7 +27,7 @@ const LoadingScreen = () => {
   return (
     <SafeAreaView className="flex items-center justify-center h-[100%] w-[100%] bg-white">
 
-      <View className='w-[100%] h-[auto] flex justify-center items-center mt-20'>
+      <View className='w-[100%] h-[auto] flex justify-center items-center mt-14'>
         <Image 
           source={require("../assets/get-started.png")}
           className="w-[80%] h-[auto] flex justify-center items-center m-2"
@@ -34,19 +38,27 @@ const LoadingScreen = () => {
         </Text>
       </View>
 
-      <View className='flex justify-start bg-white border-blue-600 border-10 w-[90%] h-[auto] mt-10 mb-5 rounded-full p-4'>
+      <TouchableOpacity className='flex justify-start bg-white border-blue-400 border-2 w-[90%] h-[auto] mt-10 mb-5 rounded-full p-4'>
         <Text className="text-2xl font-bold text-center text-black">
+            Login
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity 
+        className='flex justify-start mb-10 bg-blue-400 w-[90%] h-[auto] rounded-full p-4'
+        onPress={handlePress}
+      >
+        <Text className="text-2xl font-bold text-center text-white">
             Get Started
         </Text>
-      </View>
+      </TouchableOpacity>
 
-      <View className='flex justify-start mb-10 bg-blue-400 w-[90%] h-[auto] rounded-full p-4'>
-        <Text className="text-2xl font-bold text-center text-black">
-            Get Started
-        </Text>
-      </View>
-
-      <Text>By Signing up you agree to the terms of use and privacy policy</Text>
+      <Text className='mx-16 text-center font-medium'>
+        By Signing up you agree to the 
+        <Text className='text-blue-800'> Terms of use </Text> 
+        and 
+        <Text className='text-blue-800'> Privacy policy</Text>
+      </Text>
 
       {/* <Progress.Circle size={60} indeterminate={true} color='black' /> */}
 
