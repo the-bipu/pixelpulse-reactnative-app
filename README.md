57c7b973245946d14291d62d25b93f5afee4052e
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-01-10 16:26:27 +0530

    update HomeScreen

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # DoneWithIt
  First React Native Application
+
+if any page needed backgroud as a whole page bg.
+
+```
+import { View, Text, SafeAreaView, ImageBackground } from 'react-native'
+import React, { useEffect, useLayoutEffect } from 'react'
+import * as Animatable from 'react-native-animatable'
+// import * as Progress from 'react-native-progress'
+import { useNavigation } from '@react-navigation/native'
+
+const LoadingScreen = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+          headerTitle:"Loading",
+          headerShown: false,
+        });
+    }, []);
+      
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         navigation.navigate("Home");
+    //     }, 4000);
+    // }, []);
+
+  return (
+    <SafeAreaView className="flex items-center justify-center">
+        <ImageBackground
+            source={require("../assets/pink-tree.jpg")}
+            className="w-full h-full flex justify-center items-center"
+        >
+          <Text className="flex-1 text-xl font-bold text-center text-black">
+            Hey Buddy !! 🚀
+          </Text>
+
+          <View className='flex justify-start mb-10 bg-blue-400 w-[90%] h-[auto] m-5 rounded-xl p-5'>
+            <Text className="text-2xl font-bold text-center text-black">
+                Get Started
+            </Text>
+          </View>
+        </ImageBackground>
+
+      {/* <Progress.Circle size={60} indeterminate={true} color='black' /> */}
+
+    </SafeAreaView>
+  )
+}
+
+export default LoadingScreen
+```
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,27 +2,29 @@ import { View, Text, Image, TextInput, ScrollView, ImageBackground } from 'react
 import { SafeAreaView } from 'react-native-safe-area-context';
 import React, { useLayoutEffect, useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { 
-    UserIcon,
-    ChevronDownIcon,
-    MagnifyingGlassIcon,
-    CogIcon,
- } from 'react-native-heroicons/outline';
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  CogIcon,
+  HomeIcon,
+} from 'react-native-heroicons/outline';
 
 
 export default function HomeScreen() {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-          headerTitle:"Anupma",
-          headerShown: false,
-        });
-      }, []);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: "Anupma",
+      headerShown: false,
+    });
+  }, []);
 
   return (
     <SafeAreaView className="bg-white w-full h-full">
 
+      <ScrollView classNamew='w-full h-full'>
         <ImageBackground
           source={require("../assets/memories-home.jpg")}
           className="w-full h-[300px]"
@@ -32,53 +34,103 @@ export default function HomeScreen() {
           </View>
         </ImageBackground>
 
-        <ScrollView 
+        <ScrollView
           horizontal
           className='w-auto h-[100px] bg-[#ebedee] flex flex-row flex-initial p-2'
         >
           <ImageBackground
             source={require("../assets/memories-home.jpg")}
-            className="h-[80px] w-[80px] mr-2 rounded-xl overflow-hidden flex justify-end"
+            className="h-20 w-20 mr-2 rounded-xl overflow-hidden flex justify-end"
           >
             <Text className='text-white font-medium p-2'>January</Text>
           </ImageBackground>
 
           <ImageBackground
             source={require("../assets/memories-home.jpg")}
-            className="h-[80px] w-[80px] mr-2 rounded-xl overflow-hidden flex justify-end"
+            className="h-20 w-20 mr-2 rounded-xl overflow-hidden flex justify-end"
           >
             <Text className='text-white font-medium p-2'>February</Text>
           </ImageBackground>
 
           <ImageBackground
             source={require("../assets/memories-home.jpg")}
-            className="h-[80px] w-[80px] mr-2 rounded-xl overflow-hidden flex justify-end"
+            className="h-20 w-20 mr-2 rounded-xl overflow-hidden flex justify-end"
           >
             <Text className='text-white font-medium p-2'>March</Text>
           </ImageBackground>
 
           <ImageBackground
             source={require("../assets/memories-home.jpg")}
-            className="h-[80px] w-[80px] mr-2 rounded-xl overflow-hidden flex justify-end"
+            className="h-20 w-20 mr-2 rounded-xl overflow-hidden flex justify-end"
           >
             <Text className='text-white font-medium p-2'>April</Text>
           </ImageBackground>
 
           <ImageBackground
             source={require("../assets/memories-home.jpg")}
-            className="h-[80px] w-[80px] mr-2 rounded-xl overflow-hidden flex justify-end"
+            className="h-20 w-20 mr-2 rounded-xl overflow-hidden flex justify-end"
           >
             <Text className='text-white font-medium p-2'>June</Text>
           </ImageBackground>
 
           <ImageBackground
             source={require("../assets/memories-home.jpg")}
-            className="h-[80px] w-[80px] mr-2 rounded-xl overflow-hidden flex justify-end"
+            className="h-20 w-20 mr-2 rounded-xl overflow-hidden flex justify-end"
           >
             <Text className='text-white font-medium p-2'>July</Text>
           </ImageBackground>
 
         </ScrollView>
+
+        <View className="p-4">
+          <Text className="font-bold text-black text-xl mb-2">Pinned</Text>
+          <View className='w-full h-auto flex-row flex-wrap gap-4'>
+            <Image source={require("../assets/memories/memories-1.png")} className="h-28 w-28" />
+            <Image source={require("../assets/memories/memories-2.png")} className="h-28 w-28" />
+            <Image source={require("../assets/memories/memories-3.png")} className="h-28 w-28" />
+            <Image source={require("../assets/memories/memories-4.png")} className="h-28 w-28" />
+            <Image source={require("../assets/memories/memories-5.png")} className="h-28 w-28" />
+            <Image source={require("../assets/memories/memories-6.png")} className="h-28 w-28" />
+            <Image source={require("../assets/memories/memories-7.png")} className="h-28 w-28" />
+            <Image source={require("../assets/memories/memories-8.png")} className="h-28 w-28" />
+          </View>
+        </View>
+
+        <View className="p-4">
+          <Text className="font-bold text-black text-xl mb-2">Album</Text>
+
+          <View className='w-full h-auto flex-row flex-wrap gap-2'>
+
+            <ImageBackground
+              source={require("../assets/memories/memories-2.png")}
+              className="h-28 w-28 mr-2 rounded-xl overflow-hidden flex justify-end"
+            >
+              <Text className='text-white font-medium p-2'>1. First Meet</Text>
+            </ImageBackground>
+
+            <ImageBackground
+              source={require("../assets/memories/memories-3.png")}
+              className="h-28 w-28 mr-2 rounded-xl overflow-hidden flex justify-end"
+            >
+              <Text className='text-white font-medium p-2'>2. Breakup</Text>
+            </ImageBackground>
+
+            <ImageBackground
+              source={require("../assets/memories/memories-4.png")}
+              className="h-28 w-28 mr-2 rounded-xl overflow-hidden flex justify-end"
+            >
+              <Text className='text-white font-medium p-2'>3. Pachup</Text>
+            </ImageBackground>
+
+          </View>
+        </View>
+
+
+      </ScrollView>
+
+      <View className=" w-full h-16 bg-slate-200 sticky bottom-0">
+        <HomeIcon color="black" fill="black" size={42} />
+      </View>
 
     </SafeAreaView>
   )
